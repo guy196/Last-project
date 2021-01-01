0f8515604eae2eea2bea7f26a39f2285aa15096c
guy196 <guy.shalev01@gmail.com>
2021-01-01 17:16:42 +0200

    :)

--- 3D shooter - Oz sasson/Assets/scripts/gunscript.cs
+++ 3D shooter - Oz sasson/Assets/scripts/gunscript.cs
@@ -4,28 +4,81 @@ using UnityEngine;
 
 public class gunscript : MonoBehaviour
 {
-    public float damage = 10;
+    public float damage = 50f;
 
     public Camera fpscam;
+
+    public int maxammo = 10;
+
+    private int currentAmmo;
+
+    public float reloadTime = 30f;
+
+    private bool isReloding = false;    
+    public ParticleSystem mazlleflush;
+
+    public Animator animator;
     void Start()
     {
-        
+        currentAmmo = maxammo;
+    }
+
+    private void OnEnable()
+    {
+        isReloding = false;
+        animator.SetBool("realoding", false);
     }
 
     void Update()
     {
+        if (isReloding)
+            return;
+
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetButtonDown("Fire1"))
         {
             shoot();
         }
+
+    }
+
+    IEnumerator Reload()
+    {
+        isReloding = true;
+        Debug.Log("reloading");
+
+        animator.SetBool("realoding", true);
+
+        yield return new WaitForSeconds(reloadTime -2.5f);
+
+        animator.SetBool("realoding", false);
+        yield return new WaitForSeconds(1f);
+
+        currentAmmo = maxammo;
+        isReloding = false;
+
+
+
     }
 
     void shoot()
     {
         RaycastHit hit;
+        mazlleflush.Play();
+
+        currentAmmo--;
         if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit))
         {
             Debug.Log(hit.transform.name);
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.TakeDamege(20);
+            }            
         }
     }
 }
