4ff46c373427108fe685851fb468b2f822816b13
guy196 <guy.shalev01@gmail.com>
2021-01-08 15:53:24 +0200

    Player & room list

--- 3D shooter - Oz sasson/Assets/MultiPlayer/Scripts/Luncher.cs
+++ 3D shooter - Oz sasson/Assets/MultiPlayer/Scripts/Luncher.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
+using System.Linq;
+
 public class Luncher : MonoBehaviourPunCallbacks
 {
+    public static Luncher Instance;
     [SerializeField] TMP_InputField roomnameInputField;
     [SerializeField] TMP_Text errorText;
     [SerializeField] TMP_Text roomNameText;
+    [SerializeField] Transform roomlistContent;
+    [SerializeField] GameObject roomlistItemPrefab;
+    [SerializeField] GameObject PlayerlistItemPrefab;
+    [SerializeField] Transform PlayerlistContent;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        Instance = this;
+    }
     void Start()
     {
         Debug.Log("connecting to Master");
@@ -27,6 +39,7 @@ public class Luncher : MonoBehaviourPunCallbacks
     {
         MenuManager.Instance.OpenMenu("title");
         Debug.Log("Joined lobby");
+        PhotonNetwork.NickName = "Player" + Random.Range(0, 1000).ToString("0000");
     }
 
     public void CreateRoom()
@@ -43,11 +56,51 @@ public class Luncher : MonoBehaviourPunCallbacks
     {
         roomNameText.text = PhotonNetwork.CurrentRoom.Name;
         MenuManager.Instance.OpenMenu("room");
+        Player[] players = PhotonNetwork.PlayerList;
+
+        for (int i = 0; i < players.Count(); i++)
+        {
+            Instantiate(PlayerlistItemPrefab, PlayerlistContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+        }   
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         errorText.text = "room creation Failed" + message;
         MenuManager.Instance.OpenMenu("error");
+    }
+
+    public void leaveroom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("title");
+    }
+
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("loading");
+
+    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (Transform trans in roomlistContent)
+        {
+            Destroy(trans.gameObject);
+        }
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomlistItemPrefab, roomlistContent).GetComponent<roomlistItem>().Setup(roomList[i]);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(PlayerlistItemPrefab, PlayerlistContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
     }
 }
---
+++ 3D shooter - Oz sasson/Assets/roomlistItem.cs
@@ -0,0 +1,21 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class roomlistItem : MonoBehaviour
+{
+	[SerializeField] TMP_Text text;
+
+	RoomInfo info;
+	public void	Setup(RoomInfo _info)
+	{
+		info = _info;
+		text.text = info.Name;
+	}
+
+	public void onClick()
+	{
+		Luncher.Instance.JoinRoom(info);
+	}
+}
